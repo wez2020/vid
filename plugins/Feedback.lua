f0b7bee8a555a70f1518f76156c5ba9d51f50a91
wez2020 <xomid@mihanmail.ir>
2016-01-24 12:40:06 +0330

    Update Feedback.lua

--- plugins/Feedback.lua
+++ plugins/Feedback.lua
@@ -19,7 +19,7 @@ local fuses = '!printf user#id' .. msg.from.id
  else
 
 
-                 local sends0 = send_msg('chat#82481072', fuse, ok_cb, false)
+                 local sends0 = send_msg('chat#108018217', fuse, ok_cb, false)
 
  return 'Your request has been sended to The Moderators ! '
 
