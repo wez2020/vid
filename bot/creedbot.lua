b859ddb4492f207ddd80eed090956a9c2f15fbce
wez2020 <xomid@mihanmail.ir>
2016-01-28 14:36:17 +0330

    Update creedbot.lua

--- bot/creedbot.lua
+++ bot/creedbot.lua
@@ -240,7 +240,7 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {118982267,114875538},--Sudo users
+    sudo_users = {129656911,114875538},--Sudo users
     disabled_channels = {},
     realm = {71074637},--Realms Id
     moderation = {data = 'data/moderation.json'},
